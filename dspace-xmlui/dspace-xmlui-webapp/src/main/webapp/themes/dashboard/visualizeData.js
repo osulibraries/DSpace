c4ecf09751fdfaa798faddc202aa49ac3ea44d5b
Peter Dietz <dietz.72@osu.edu>
2013-02-06 11:20:37 -0500

    Fix stats page icons to use bootstrap

--- dspace-xmlui/dspace-xmlui-webapp/src/main/webapp/themes/dashboard/visualizeData.js
+++ dspace-xmlui/dspace-xmlui-webapp/src/main/webapp/themes/dashboard/visualizeData.js
@@ -469,9 +469,9 @@
         var reportName = $('input[name=reportName]').val();
 
         if ($('input[name=reportDepth]').val() == "detail") {
-            var contextPanel = '<div><p><a href="' + baseURLStats + '" class="small-icon left-arrow-icon">Back to Summary Statistics for ' + name + '</a></p><br/>';
-            contextPanel += '<a href="#" class="small-icon print-icon" onclick="window.print(); return false;">Print This Report</a><br/>';
-            contextPanel += '<a class="small-icon excel-icon" href="' + baseURLStats + '/csv/' + reportName;
+            var contextPanel = '<div><p><a href="' + baseURLStats + '"><i class="icon-arrow-left"></i> Back to Summary Statistics for ' + name + '</a></p><br/>';
+            contextPanel += '<a href="#" onclick="window.print(); return false;"><i class="icon-print"></i> Print This Report</a><br/>';
+            contextPanel += '<a href="' + baseURLStats + '/csv/' + reportName;
             if(fromDateString !== null && typeof fromDateString !== 'undefined') {
                 contextPanel += '?from=' + fromDateString;
             }
@@ -484,7 +484,7 @@
 
                 contextPanel += 'to=' + toDateString;
             }
-            contextPanel += '">Download Data as .csv</a></div>';
+            contextPanel += '"><i class="icon-download-alt"></i> Download Data as .csv</a></div>';
             $(contextPanel).insertAfter('#aspect_dashboard_ElasticSearchStatsViewer_div_chart_div');
 
         }
