459ef639afcbc082622ecbf3916f08264edad6c5
Peter Dietz <dietz.72@osu.edu>
2013-04-11 15:31:14 -0400

    KB-396 Add clarify notes that data is only from 2008, onward

--- dspace-xmlui/dspace-xmlui-webapp/src/main/webapp/themes/dashboard/visualizeData.js
+++ dspace-xmlui/dspace-xmlui-webapp/src/main/webapp/themes/dashboard/visualizeData.js
@@ -545,8 +545,8 @@
         if ($('input[name=reportDepth]').val() == "summary") {
             $('<p>'+timeRangeString+' <a href="'+ baseURLStats + '/itemsAdded">For Custom Report Generator.</a></p>').insertBefore('#aspect_dashboard_ElasticSearchStatsViewer_table_itemsAddedGrid');
             $('<p>'+timeRangeString+' <a href="'+ baseURLStats + '/filesAdded">For Custom Report Generator.</a></p>').insertBefore('#aspect_dashboard_ElasticSearchStatsViewer_table_filesInContainer-grid');
-            $('<h3>Number of File Downloads for ' + name + '</h3>'+timeRangeString+' <a href="'+ baseURLStats + '/fileDownloads">For Custom Report Generator.</a>').insertBefore('#dspaceChart_downloadsMonthly');
-            $('<h3>Countries with most Downloads ' + name + '</h3>'+timeRangeString+' <a href="'+ baseURLStats + '/topCountries">For Custom Report Generator.</a> <p>Note: Results limited to Top 150 Countries.</p> <h5 style="margin-left: 150px">Countries with most Downloads</h5>').insertBefore('#dspaceChart_topCountries');
+            $('<h3>Number of File Downloads for ' + name + '</h3>'+timeRangeString+' <a href="'+ baseURLStats + '/fileDownloads">For Custom Report Generator.</a> <p>Note: Knowledge Bank download data available 2008-</p>').insertBefore('#dspaceChart_downloadsMonthly');
+            $('<h3>Countries with most Downloads ' + name + '</h3>'+timeRangeString+' <a href="'+ baseURLStats + '/topCountries">For Custom Report Generator.</a> <p>Note: Knowledge Bank download data available 2008- Results limited to Top 150 Countries.</p> <h5 style="margin-left: 150px">Countries with most Downloads</h5>').insertBefore('#dspaceChart_topCountries');
             $('<p>'+timeRangeString+' <a href="'+ baseURLStats + '/topUSCities">For Custom Report Generator.</a></p>').insertBefore('#dspaceChart_topUSCities');
             $('<p><a href="'+ baseURLStats + '/topDownloads">For Custom Report Generator.</a></p>').insertBefore('#aspect_dashboard_ElasticSearchStatsViewer_table_facet-Bitstream');
         }
@@ -574,8 +574,13 @@
 
             // Add a note to the topCountries detail page about limit 150 countries
             if ($('input[name=reportName]').val() == 'topCountries') {
-                var countriesNote = '<div class="alert alert-info"><b>Note:</b> Results limited to Top 150 Countries</div>';
+                var countriesNote = '<div class="alert alert-info"><b>Note:</b> Knowledge Bank download data available 2008- Results limited to Top 150 Countries</div>';
                 $(countriesNote).insertBefore('#aspect_dashboard_ElasticSearchStatsViewer_div_chart_div');
+            }
+
+            if ($('input[name=reportName]').val() == 'fileDownloads') {
+                var note = '<div class="alert alert-info"><b>Note:</b> Knowledge Bank download data available 2008-</div>';
+                $(note).insertBefore('#aspect_dashboard_ElasticSearchStatsViewer_div_chart_div');
             }
 
         }
