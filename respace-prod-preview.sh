19e35219a23cb72622309ebce5035e11b78b45ff
libkbhelp <libkbhelp@lists.osu.edu>
2016-12-09 17:57:39 -0500

    Add prod-preview respace

---
+++ respace-prod-preview.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -e
+## Script to pull, build, and redeploy a DSpace
+
+
+site="OSU ProdPreview"
+configDir="/dspace/config"
+sourceDir="/dspace-source/osukb56"
+tomcatStop="/opt/tomcat/bin/shutdown.sh"
+tomcatStart="/opt/tomcat/bin/startup.sh"
+mavenCommand="/bin/mvn"
+
+echo "Rebuild and Redeploy $site"
+
+cd $configDir
+if [[ -n $(git status --porcelain) ]]; then
+   echo "Instance config Git repository is dirty, aborting. PWD: $PWD"
+   exit 1
+fi
+
+echo "Instance config Git Repository is clean, continuing"
+
+
+cd $sourceDir
+if [[ -n $(git status --porcelain) ]]; then
+   echo "DSpace-Source Git repository is dirty, aborting. PWD: $PWD"
+   exit 1
+fi
+
+echo "DSpace-Source Git Repository is clean, continuing"
+
+
+
+cd $sourceDir
+git pull
+$mavenCommand clean package
+cd dspace/target/dspace-installer/
+
+$tomcatStop
+sleep 10
+
+
+ant -Dconfig=$configDir/dspace.cfg update
+
+cd $configDir
+git reset HEAD --hard
+
+$tomcatStart
